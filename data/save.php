528051678e6366a8e455a8bbdb40cdc8fb74395f
SongXinxing <2845639@qq.com>
2016-08-01 18:28:47 +0800

    Update save.php

--- data/save.php
+++ data/save.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin:https://songxinxing.github.io/phone_form/xinmei.html');
+/*header('Access-Control-Allow-Origin:https://songxinxing.github.io/phone_form/xinmei.html');
 $userName=$_REQUEST['userName'];
 $IDNumber=$_REQUEST['IDNumber'];
 $telPhone=$_REQUEST['telPhone'];
@@ -11,4 +11,6 @@ $recommend_telPhone=$_REQUEST['recommend_telPhone'];
 $addr=$_REQUEST['addr'];
 
 echo $userName.'__'.$IDNumber.'__'.$telPhone.'__'.$email.'__'.$company.'__'.$company_L.'__'.$recommend_userName.'__'.$recommend_telPhone.'__'.$addr;
+*/
+echo "myPHP"
 ?>
